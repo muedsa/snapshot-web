5686aa5547aa59c0d03bed9ac4cc32fea1768941
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-01-14 11:20:41 +0800

    refactor: some changes with ktor 3.x

--- src/main/kotlin/com/muedsa/snapshot/web/plugins/CallLogging.kt
+++ src/main/kotlin/com/muedsa/snapshot/web/plugins/CallLogging.kt
@@ -2,7 +2,7 @@ package com.muedsa.snapshot.web.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.plugins.callid.*
-import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.calllogging.CallLogging
 import org.slf4j.event.Level
 
 fun Application.configureCallLogging() {
