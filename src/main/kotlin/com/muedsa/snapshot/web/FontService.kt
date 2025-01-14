531248bdb3fa4bf01eabe04d12a21f0b5412c682
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-01-14 11:31:14 +0800

    update(web): display font names more clearly

--- src/main/kotlin/com/muedsa/snapshot/web/FontService.kt
+++ src/main/kotlin/com/muedsa/snapshot/web/FontService.kt
@@ -3,9 +3,11 @@ package com.muedsa.snapshot.web
 import com.muedsa.geometry.EdgeInsets
 import com.muedsa.snapshot.SnapshotPNG
 import com.muedsa.snapshot.paint.text.TextPainter
+import com.muedsa.snapshot.paint.text.TextSpan
 import com.muedsa.snapshot.paint.text.TextStyle
 import com.muedsa.snapshot.rendering.flex.CrossAxisAlignment
 import com.muedsa.snapshot.widget.*
+import com.muedsa.snapshot.widget.text.RichText
 import com.muedsa.snapshot.widget.text.Text
 import org.jetbrains.skia.Color
 import org.jetbrains.skia.FontMgr
@@ -22,15 +24,24 @@ object FontService {
                     for (i in 0 until familiesCount) {
                         val familyName = FontMgr.default.getFamilyName(i)
                         val familyNameArr = listOf(familyName)
-                        Text(
-                            text = familyName,
-                            style = TextStyle(
-                                color = Color.RED,
-                                fontSize = 40f,
-                                fontStyle = FontStyle.BOLD,
-                                fontFamilies = familyNameArr
+                        RichText {
+                            TextSpan(
+                                text = familyName,
+                                style = TextStyle(
+                                    color = Color.RED,
+                                    fontSize = 40f,
+                                    fontStyle = FontStyle.BOLD,
+                                    fontFamilies = familyNameArr
+                                )
                             )
-                        )
+                            TextSpan(
+                                text = "  ($familyName)",
+                                style = TextStyle(
+                                    color = Color.BLACK,
+                                    fontSize = 35f,
+                                )
+                            )
+                        }
                         Text(
                             text = "原神, 启动！\uD83E\uDD23\uD83E\uDD23\n原神, 啓動！\uD83E\uDD23\uD83E\uDD23\nGenshin Impact, Launch！\uD83E\uDD23\uD83E\uDD23",
                             style = TextStyle(
